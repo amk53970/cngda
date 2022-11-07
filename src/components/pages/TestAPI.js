be3e408c491f94598a446e6c6c7f5e5ce994de89
amk53970 <amk53970@ucmo.edu>
2022-11-07 12:44:28 -0600

    upated

--- src/components/pages/TestAPI.js
+++ src/components/pages/TestAPI.js
@@ -5,7 +5,7 @@ function TestAPI() {
         <div>
             <F1>
             <Form.Item wrapperCol={{offset: 8}} name="JWTRefreshButton">
-                        <Button type="primary" style={{width: 150, height: 45}} onClick={"refreshJWT()"}>
+                        <Button type="primary" style={{width: 150, height: 45}} onClick={null}>
                             JWTRefresh
                         </Button>
                     </Form.Item>
@@ -22,11 +22,3 @@ const F1 = styled.div`
     align-items: center;
     justify-content: center;
 `
-function refreshJWT(){
-    fetch("https://gsuhsjh6g667xscsgbsrtrheba0rrdla.lambda-url.us-east-1.on.aws/")
-    .then(response => response.json())
-    .then((data) => {
-        window.open(data.news[0].url);
-    })
-    .catch((error) => console.error("oops:",error));
-}
